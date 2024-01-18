df746a8308d234ed08d76ec9b7f4d00cd5af147d
REN7000K-LF-403 <ylf22@mails.tsinghua.edu.cn>
2024-01-18 19:51:13 +0800

    实现从外部的csv文件中载入参数，并进行保存

--- test/untitled.m
+++ test/untitled.m
@@ -1,30 +1,12 @@
-function save_file_scan_result(app, save_file_name, File_scan_results)
-    try
-        % 先检测要保存的内容是否为空
-        if isempty(File_scan_results)
-            return
-        end
-        % 获取当前时间戳
-        time_stamp = datestr(now, 'yyyymmddHHMMSS');
-        % 生成文件名
-        file_name = [save_file_name, time_stamp, '.txt'];
-        % 生成文件路径
-        if app.save_resul_path.Value(end) == '\'
-            file_path = fullfile(app.save_resul_path.Value, file_name);
-        else
-            file_path = fullfile(app.save_resul_path.Value, '\', file_name);
-        end
 
-        % 保存文件内容
-        fid = fopen(file_path, 'w');
-        for i = 1:length(File_scan_results)
-            fprintf(fid, '%s\n', File_scan_results{i});
-        end
-        fclose(fid);
+% 设置原始 CSV 文件路径和新文件名
+originalCsvFilePath = fullfile('D:\DTOOL\科研\课题-升降速异常检测\智能故障检测_离线扫描定位软件\AMB_FSL_3.0\SetData.csv');
+newCsvFileName = 'SetData_temp.csv';
 
-        msgbox('扫描结果保存成功');
-    catch
-        msgbox('扫描结果保存失败');
-    end
-    
-end
+tempCsvFilePath = 'temp_SetData_temp.csv';
+
+% 复制原始 CSV 文件并改名
+copyfile(originalCsvFilePath, tempCsvFilePath, 'f');
+
+% 移动新 CSV 文件到 .\config 文件夹
+movefile(tempCsvFilePath, fullfile('D:\DTOOL\科研\课题-升降速异常检测\智能故障检测_离线扫描定位软件\AMB_FSL_3.0\config\', newCsvFileName), 'f');
